8a166cac8825b190a142fcbb8a95e09f4350bcf3
Bruno Verachten <gounthar@gmail.com>
2021-10-20 09:00:12 +0000

    Switched to Adelie 1.0-rc2

--- adelie.sh
+++ adelie.sh
@@ -3,15 +3,16 @@
 # Creates a fully-featured Adélie Linux Docker image.
 # 20180916 | Depends: { curl, xz }
 
-aver=1.0-beta
-arev=4
+aver=1.0
+arev=rc2
 arch=aarch64
 amir=https://distfiles.adelielinux.org/adelie/
+#https://distfiles.adelielinux.org/adelie/
 
-tags=mini-${arch}-${aver}${arev}
+tags=mini-${arch}-${aver}-${arev}
 
 #https://distfiles.adelielinux.org/adelie/1.0-beta4/iso/adelie-rootfs-mini-aarch64-1.0-beta4.txz
-https://distfiles.adelielinux.org/adelie//1.0-beta4/iso/adelie-rootfs-mini-aarch64-1.0-beta4.txz
-echo ${amir}/${aver}${arev}/iso/adelie-rootfs-${tags}.txz
-curl -S ${amir}/${aver}${arev}/iso/adelie-rootfs-${tags}.txz \
-    | docker import - zvio/adelie:${aver}
+echo https://distfiles.adelielinux.org/adelie//1.0/iso/rc2/adelie-rootfs-mini-aarch64-1.0-rc2.txz
+#https://distfiles.adelielinux.org/adelie//1.0-beta4/iso/adelie-rootfs-mini-aarch64-1.0-beta4.txz
+echo ${amir}/${aver}/iso/${arev}/adelie-rootfs-${tags}.txz
+docker import ${amir}/${aver}/iso/${arev}/adelie-rootfs-${tags}.txz gounthar/adelie:${aver}
